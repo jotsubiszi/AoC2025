8eccafe39b6123e3f077fc9e8d88f7d63b2c3d14
Szymon <64400458+jotsubiszi@users.noreply.github.com>
2025-12-15 18:03:00 +0100

    Day one

---
+++ day1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    char char_input;
+    int int_input;
+    int counter = 0;
+    int dial_pointing_at = 50;
+    
+    while(!(char_input == 'A')){
+        
+        scanf("%c%d", &char_input, &int_input);
+        
+        if(char_input == 'R'){
+            
+            dial_pointing_at += int_input;
+            while(dial_pointing_at > 99){
+                
+                dial_pointing_at -= 100;
+                ++ counter;
+            }
+            
+        }else if(char_input == 'L'){
+            
+            if(dial_pointing_at == 0) counter --;
+            
+            dial_pointing_at -= int_input;
+            while(dial_pointing_at < 1){
+                
+                counter ++;
+                dial_pointing_at += 100;
+            }
+            
+            if(dial_pointing_at == 0) counter ++;
+            
+        }else continue;
+        
+        
+        printf("-------------------------------\n%c%d\nCounter: %d\nDial: %d\n", char_input, int_input, counter, dial_pointing_at);
+        
+    }
+    return 0;
+}
